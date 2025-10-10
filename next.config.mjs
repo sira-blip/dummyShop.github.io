3fac3454252ae8471e174791b2a978bdaff4aaf1
ouz.chaimaa@gmail.com <c.ouzougagh@enim.ac.ma>
2025-10-10 15:40:48 +0100

    add out

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,11 @@
-export default { reactStrictMode: true };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  reactStrictMode: true,
+
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
