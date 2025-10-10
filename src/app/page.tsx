1c5318e13e3d3e892190c0be28ff99ae37a9dc97
ouz.chaimaa@gmail.com <c.ouzougagh@enim.ac.ma>
2025-10-10 14:33:43 +0100

    config to deploy to git pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
     <div className="card">
-      <h1 className="text-2xl font-semibold mb-2">Starter Skeleton – Visionyze / DummyJSON</h1>
+      <h1 className="text-2xl font-semibold mb-2">
+        Starter Skeleton – Visionyze / DummyJSON
+      </h1>
       <p className="text-sm text-gray-600 dark:text-gray-300">
-        Aucune logique implémentée. À vous d'écrire les appels API, la pagination, la recherche, etc.
+        Aucune logique implémentée. À vous d'écrire les appels API, la
+        pagination, la recherche, etc.
       </p>
     </div>
   );
