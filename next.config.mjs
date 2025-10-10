ee7cd56e15e61ccca9240693a80bf3d54b8f8999
ouz.chaimaa@gmail.com <c.ouzougagh@enim.ac.ma>
2025-10-10 15:01:30 +0100

    Add GitHub Pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
+export default { reactStrictMode: true };
